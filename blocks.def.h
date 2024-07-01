797dde811f8640d0d1252e411ab6d08e5250ca58
LuckusMI <luckusmibus@gmail.com>
2024-07-01 14:49:29 +0400

    Fork Bar

--- blocks.def.h
+++ blocks.def.h
@@ -1,12 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+    {"┇ ", "xset -q|grep LED| awk '{ if (substr ($10,5,1) == 1) print \"[RU]\"; else print \"[EN]\"; }'", 0, 1},
+    {" ", "echo \"$(cat /sys/class/power_supply/BAT0/capacity)%\"", 15, 0},
+    {" ", "curl -s 'wttr.in/Tashkent?format=%t'", 300, 0},
+/*	{" ", "date '+%d.%m'", 60, 0}, */
+	{" ", "date '+%H:%M:%S'", 5, 0},
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char delim[] = " | ";
-static unsigned int delimLen = 5;
-d
+static char delim[] = "  ┇  ";
+static unsigned int delimLen = 7;
