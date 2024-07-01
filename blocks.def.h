f13ed7a7356a3059cdeeabe6d0258bd47e1c5af4
LuckusMI <luckusmibus@gmail.com>
2024-07-01 15:22:30 +0400

    Update

--- blocks.def.h
+++ blocks.def.h
@@ -3,9 +3,9 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
     {"┇ ", "xset -q|grep LED| awk '{ if (substr ($10,5,1) == 1) print \"[RU]\"; else print \"[EN]\"; }'", 0, 1},
     {" ", "echo \"$(cat /sys/class/power_supply/BAT0/capacity)%\"", 15, 0},
-    {" ", "curl -s 'wttr.in/Tashkent?format=%t'", 300, 0},
+    {" ", "curl -s 'wttr.in/Ulyanovsk?format=%t'", 300, 0},
 /*	{" ", "date '+%d.%m'", 60, 0}, */
-	{" ", "date '+%H:%M:%S'", 5, 0},
+	{" ", "date '+%H:%M:'", 5, 0},
 
 };
 
